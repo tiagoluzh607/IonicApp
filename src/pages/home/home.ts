129fa7bf09324bb24a72ec9bd1d9b6b9510b3120
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-19 16:07:50 -0300

    Exibindo Mensagem ao Usuario caso erro no get

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Carro } from "../../modelos/carro";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: 'page-home',
@@ -11,16 +12,31 @@ export class HomePage {
 
   public carros: Carro[];
 
-  constructor(public navCtrl: NavController, private _http: HttpClient, private _loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, private _http: HttpClient, private _loadingCtrl: LoadingController, private _alertCtrl : AlertController) {
     
-    let loading = this._loadingCtrl.create({content: 'Aguarde o carregamento dos carros...'}); //faz um componente de carregamento
+    let loading = this._loadingCtrl.create({content: 'Carregandos carros...'}); //faz um componente de carregamento
     loading.present(); // starta o componente de carregamento
 
-    this._http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
-      .subscribe((carros) =>{
-          this.carros = carros;
-          loading.dismiss(); //desativar o componente loading quando terminamos de carregar
-      });
+    this._http.get<Carro[]>('http://localhost:8080/api/carro/listaTodosx')
+      .subscribe(
+            (carros) =>{
+
+                this.carros = carros;
+                loading.dismiss(); //desativar o componente loading quando terminamos de carregar
+            },
+            (err: HttpErrorResponse)=>{
+
+                console.log(err);
+                loading.dismiss();
+
+                this._alertCtrl.create({
+                    title: 'Falha na conexão', 
+                    subTitle: 'Não foi possível carregar a lista de carros Tente Novamente mais tarde',
+                    buttons: [{text: 'Ok'}]
+                }).present();
+
+            }
+        );
 
   }
 
