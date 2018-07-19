b94f97a1a909cfbddc460d09c6a46e4af27ac192
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-19 14:16:50 -0300

    Adicionando uma lista de Carros

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public carros;
+
   constructor(public navCtrl: NavController) {
+    this.carros = [
+        { nome: "Azera V6", preco: 85000},
+        { nome: "Onix 1.6", preco: 35000},
+        { nome: "Fiesta 2.0", preco: 52000},
+        { nome: "C3 1.0", preco: 22000},
+        { nome: "Uno Fire", preco: 11000},
+        { nome: "Sentra 2.0", preco: 53000},
+        { nome: "Astra Sedan", preco: 39000}
+      ];
+
+
 
   }
 
