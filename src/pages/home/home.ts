7fc189eaea05fd0176bc576b8c666244db6450e1
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-19 15:49:29 -0300

    Inserindo elemento de carregamento na pagina

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Carro } from "../../modelos/carro";
 
 @Component({
@@ -11,13 +11,16 @@ export class HomePage {
 
   public carros: Carro[];
 
-  constructor(public navCtrl: NavController, private _http: HttpClient) {
+  constructor(public navCtrl: NavController, private _http: HttpClient, private _loadingCtrl: LoadingController) {
     
+    let loading = this._loadingCtrl.create({content: 'Aguarde o carregamento dos carros...'}); //faz um componente de carregamento
+    loading.present(); // starta o componente de carregamento
+
     this._http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
       .subscribe((carros) =>{
           this.carros = carros;
-        }
-      );
+          loading.dismiss(); //desativar o componente loading quando terminamos de carregar
+      });
 
   }
 
