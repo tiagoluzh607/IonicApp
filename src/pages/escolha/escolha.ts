d285411e5a6f9f06715f31f38c8861ac348a8143
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-20 09:28:29 -0300

    Criando pagina Escolha usando ionic g

---
+++ src/pages/escolha/escolha.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the EscolhaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-escolha',
+  templateUrl: 'escolha.html',
+})
+export class EscolhaPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EscolhaPage');
+  }
+
+}
