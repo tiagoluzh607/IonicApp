60576d3e4f72a4fbe8b13799e57e1d412156ffda
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-23 11:25:44 -0300

    Crinado Http post para postar o agendamento em um Provider

---
+++ src/providers/agendamentos-service/agendamentos-service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class AgendamentosServiceProvider {
+
+  private _url : string = 'http://localhost:8080/api';
+
+  constructor(private _http: HttpClient) {
+    
+  }
+
+  agenda(agendamento){
+    return this._http.post(this._url+'/agendamento/agenda', agendamento); //retorna um observable
+  }
+
+}
