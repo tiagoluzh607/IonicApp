a0861de5060f6f57d091bb45e09d4d4d1f72328d
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-23 13:59:33 -0300

    Exibindo alerta ao enviar dados para API

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, Alert } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
 import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
 
@@ -19,17 +19,16 @@ export class CadastroPage {
   public email: string = '';
   public data: string = new Date().toISOString();
 
+  private _alerta : Alert;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private _agendamentosService: AgendamentosServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _agendamentosService: AgendamentosServiceProvider, private _alertCtrl: AlertController) {
     this.carro = this.navParams.get('carroSelecionado');
     this.precoTotal = this.navParams.get('precoTotal');
+
   }
 
   agenda(){
-    console.log(this.nome);
-    console.log(this.endereco);
-    console.log(this.email);
-    console.log(this.data);
+
 
     let agendamento = {
       nomeCliente: this.nome,
@@ -39,11 +38,21 @@ export class CadastroPage {
       precoTotal: this.precoTotal
     };
 
+    this.criaAlerta();
+
+
     this._agendamentosService.agenda(agendamento)
       .subscribe(
-        () => alert('Agendou'),
-        () => alert('deu problema')
+        () => this._alerta.setSubTitle('Agendamento realizado!').present(),
+        () => this._alerta.setSubTitle('Falha no agendamento, tente novamente mais tarde! ').present()
       );
   }
 
+
+  private criaAlerta() {
+    this._alerta = this._alertCtrl.create({
+      title: 'Aviso',
+      buttons: [{ text: 'ok' }]
+    });
+  }
 }
