11155d496d35592507795967cee44f92452fc761
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-23 10:41:45 -0300

    Cadastro Capturando dados do formulario e executando submit

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -12,10 +12,23 @@ export class CadastroPage {
   public carro: Carro;
   public precoTotal: number;
 
+  //form
+  public nome: string = '';
+  public endereco: string = '';
+  public email: string = '';
+  public data: string = new Date().toISOString();
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.carro = this.navParams.get('carroSelecionado');
     this.precoTotal = this.navParams.get('precoTotal');
   }
 
+  agenda(){
+    console.log(this.nome);
+    console.log(this.endereco);
+    console.log(this.email);
+    console.log(this.data);
+  }
 
 }
