7429e4b99a0b3aede48a80f4b4fae551bb1ca701
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-23 13:38:56 -0300

    Utilizando servico de Agendamento

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
+import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
 
 @IonicPage()
 @Component({
@@ -19,7 +20,7 @@ export class CadastroPage {
   public data: string = new Date().toISOString();
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _agendamentosService: AgendamentosServiceProvider) {
     this.carro = this.navParams.get('carroSelecionado');
     this.precoTotal = this.navParams.get('precoTotal');
   }
@@ -29,6 +30,20 @@ export class CadastroPage {
     console.log(this.endereco);
     console.log(this.email);
     console.log(this.data);
+
+    let agendamento = {
+      nomeCliente: this.nome,
+      enderecoCliente: this.endereco,
+      emailCliente: this.email,
+      modeloCarro: this.carro.nome,
+      precoTotal: this.precoTotal
+    };
+
+    this._agendamentosService.agenda(agendamento)
+      .subscribe(
+        () => alert('Agendou'),
+        () => alert('deu problema')
+      );
   }
 
 }
