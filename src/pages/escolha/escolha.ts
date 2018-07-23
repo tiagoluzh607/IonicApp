84b73651221ce0d1d4d233093dd2b2d018eeb6d7
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-23 08:51:23 -0300

    Page Escolha - Adicionando Acessorios e atualizando total de preco

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
+import { Acessorio } from '../../modelos/acessorio';
 
 /**
  * Generated class for the EscolhaPage page.
@@ -17,16 +18,30 @@ import { Carro } from '../../modelos/carro';
 export class EscolhaPage {
 
   public carro: Carro;
+  public acessorios: Acessorio[];
+  private _precoTotal: number;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams) {
 
       this.carro = this.navParams.get('carroSelecionado'); //pegando carro passado como parâmetro por outra página
+      this._precoTotal = this.carro.preco;
+      this.acessorios = [
+        { nome: 'Freio ABS', preco: 800},
+        { nome: 'Ar-condicionado', preco: 1000},
+        { nome: 'Mp3 Player', preco: 500}
+      ];
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EscolhaPage');
+  atualizaTotal(ativado: boolean, acessorio : Acessorio){
+    ativado ? //if ternario
+      this._precoTotal += acessorio.preco :
+      this._precoTotal -= acessorio.preco;
+  }
+
+  get precoTotal(){
+    return this._precoTotal;
   }
 
 }
