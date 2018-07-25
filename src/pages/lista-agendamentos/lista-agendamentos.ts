563a2ee32875f56362bf7461b8ce5fb32681fef5
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-25 14:11:22 -0300

    Criando botão e funcionalidade de reenvio

--- src/pages/lista-agendamentos/lista-agendamentos.ts
+++ src/pages/lista-agendamentos/lista-agendamentos.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { AgendamentoDaoProvider } from '../../providers/agendamento-dao/agendamento-dao';
 import { Agendamento } from '../../modelos/agendamento';
+import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
 
 @IonicPage()
 @Component({
@@ -11,8 +12,13 @@ import { Agendamento } from '../../modelos/agendamento';
 export class ListaAgendamentosPage {
 
   agendamentos: Agendamento[] = [];
+  private _alerta;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private _agendamentoDao: AgendamentoDaoProvider) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private _agendamentoDao: AgendamentoDaoProvider,
+    private _alertCtrl: AlertController,
+    private _agendamentosService: AgendamentosServiceProvider) {
   }
 
   ionViewDidLoad() {
@@ -24,6 +30,37 @@ export class ListaAgendamentosPage {
         },
         (erro: Error) => {console.log(erro.message)}
       );
+  }
+
+  reenvia(agendamento: Agendamento){
+
+       
+        this.criaAlerta();
+    
+
+        this._agendamentosService.agenda(agendamento)
+          .mergeMap((retornoAgenda) =>{ //juntando 2 observable o do servico da api com o observable que vem retornado do banco na funcao salva
+            let observableSalva = this._agendamentoDao.salva(agendamento); 
+            
+            if(retornoAgenda instanceof Error){ //se estourar um erro em salva agendamento, estoura o erro aqui no principal
+              throw retornoAgenda;   
+            }
+            return observableSalva; //se nao tiver erro retorna o observable
+            
+          }) 
+          .subscribe(
+            (sucess) => this._alerta.setSubTitle('Agendamento Reenviado!').present(),
+            (erro : Error) => this._alerta.setSubTitle(erro.message).present()
+          );
+  }
+
+  private criaAlerta() {
+    this._alerta = this._alertCtrl.create({
+      title: 'Aviso',
+      buttons: [{ 
+        text: 'ok'
+      }]
+    });
   }
 
 }
